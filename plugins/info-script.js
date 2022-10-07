a7036944818d2ab67534162bcca846f180029a10
KahfiBotz <109428066+KahfiBotz@users.noreply.github.com>
2022-10-07 14:16:47 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCTVeRHM0v0Hqv_-O8yVkFpw*`
+let str = `*Mau SC bang Chat owner*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
